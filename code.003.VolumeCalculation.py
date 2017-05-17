a286af6f32b2cdee0f18677d6c695d1688c442a7
Andre Guerra <agu3rra@gmail.com>
2017-05-17 17:49:54 -0300

    playing around with plane equations and combinatory analysis

--- code.003.VolumeCalculation.py
+++ code.003.VolumeCalculation.py
@@ -12,4 +12,24 @@ import plotly.graph_objs as go
 # Data loading
 dfCoordinates = pd.read_csv('gpsDataUTM.csv') # reads data into dataframe
 
+#-----#-----#-----#-----#-----#-----#-----#-----#-----#-----#-----#-----#-----#
 # Calculate volume of earth from the lowest measured point
+# Idea: generate training set to a Neural Network and treat it as a regression problem
+
+# Part 1: Generate training set
+# Idea: generate a set of mathematical surface functions z=f(x,y) that go by
+# close to the points we're getting from GPS. Use these funcitons to generate a
+# set of (x,y,z) points and a double integral to calculate the exact valume of
+# the solid delimited by it.
+
+# Better idea: maybe the model can generelize from general space, no need to actually find functions that have values close to our sample set.
+# The only remaining challenge is to select only a subset of points that match the input data size (random selection)
+
+# generate mathematical surface functions that have values within the [0:*_max] range
+x = np.linspace(0.0,50.0,num=200) # generate linear space for x values
+y = np.linspace(0.0,50.0,num=200)
+xGrid, yGrid = np.meshgrid(x,y) # generate mesh grid for plotting sample data
+
+z1 = xGrid+yGrid # defining a plane
+#z2 = np.sin(xGrid**2 + yGrid**2) / (xGrid**2 + yGrid**2) #
+z3 = xGrid**2 + yGrid**2 + 2*xGrid*yGrid - 1
